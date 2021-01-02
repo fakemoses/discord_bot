5457b2731f04bdc975a2fdf23b74deceafea0b09
Muizzuddin Faiz <me@muiz.my>
2021-01-02 19:17:53 +0100

    added static page

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 server.listen(PORT, () => console.log('Server running on port ', PORT))
 
 //npm run dev
