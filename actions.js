f4a6ae45e7390dc8ab8e1406895c65973a44f944
Muizzuddin Faiz <me@muiz.my>
2021-01-03 22:33:02 +0100

    removed the single input per time as the algorithm possed too much problems

--- actions.js
+++ actions.js
@@ -3,6 +3,14 @@ const redditImageFetcher = require('reddit-image-fetcher')
 
 const matmulOpt = ['-m1', '-m2']
 const errorCommand = ['Please send the correct input!', 'For more infos about the command please send !help-commands', 'Please specify input for the matrix!']
+var matrix1 = []
+var matrix2 = []
+
+var isGiven1 = false
+var isGiven2 = false
+
+var firstVal = false
+var secondVal = false
 
 async function memes() {
     let res = await redditImageFetcher.fetch({
@@ -19,89 +27,67 @@ async function memes() {
 
 function eigenval(solveCommand, msg) {
     //need an algo to simplify the inputs. Instead of posting arrays
-        //slice the eq from the main command
-        var content = msg.content
-        const con = content.slice(solveCommand.length, content.length)
-        //split the equation for the solver
-        eq = JSON.parse(con);
-        
-        let { res, imaginary, vectors } = eigen(eq)
+    //slice the eq from the main command
+    var content = msg.content
+    const con = content.slice(solveCommand.length, content.length)
+    //split the equation for the solver
+    eq = JSON.parse(con);
 
-        msg.channel.send(
-            'Eigenvalues are: ' + res
-        )
-        msg.channel.send(
-            'Imaginary Eigenvalues are: ' + imaginary
-        )
-        msg.channel.send(
-            'Eigenvectors are: ' + vectors[0]
-        )
+    let { res, imaginary, vectors } = eigen(eq)
+
+    msg.channel.send(
+        'Eigenvalues are: ' + res
+    )
+    msg.channel.send(
+        'Imaginary Eigenvalues are: ' + imaginary
+    )
+    msg.channel.send(
+        'Eigenvectors are: ' + vectors[0]
+    )
 
 }
 
 
-function matrixMultiplication(discordMessage, msg ) {
+function matrixMultiplication(discordMessage, msg) {
     //need an algo to simplify the inputs. Instead of posting arrays
-        //slice the eq from the main command
-        if (discordMessage.length > 7) {
-            var content = discordMessage
-            const con = content.slice(7, content.length)
-            //check if there's input matrix or not
-            if (discordMessage.search(matmulOpt[0]) >= 0 && discordMessage.search(matmulOpt[1]) < 0) {
+    //slice the eq from the main command
+    if (discordMessage.length > 7) {
+        var content = discordMessage
+        const con = content.slice(7, content.length)
+        //check if there's input matrix or not
+        if (discordMessage.search(matmulOpt[0]) >= 0 && discordMessage.search(matmulOpt[1]) >= 0) {
+            //both -m1 -m2 input
+            //check if both input are correct
+            const secondInputPos = con.search(matmulOpt[1])
+            const temp1 = con.slice(matmulOpt[0].length + 2, secondInputPos - 1)
+            const temp2 = con.slice(secondInputPos + matmulOpt[1].length + 1, con.length)
 
-                //only -m1 input
-                if (con.length - 1 > matmulOpt[0].length) {
+            if (temp1.length > matmulOpt[0].length && temp2.length > matmulOpt[1].length) {
+                //convert string to array
+                matrix1 = JSON.parse(temp1)
+                matrix2 = JSON.parse(temp2)
 
-                    const tempVal = con.slice(matmulOpt[0].length + 1, con.length)
-                    //split the matrix for the solver
-                    matrix1 = JSON.parse(tempVal)
-                    //console.log(matrix1)
-
-                } else {
-                    msg.channel.send(errorCommand[0] + '\n' + errorCommand[2] + '\n' + errorCommand[1])
-                }
-
-            } else if (discordMessage.search(matmulOpt[1]) >= 0 && discordMessage.search(matmulOpt[0]) < 0) {
-                //only -m2 input
-                if (con.length - 1 > matmulOpt[1].length) {
-                    const tempVal = con.slice(matmulOpt[1].length + 1, con.length)
-                    //split the matrix for the solver
-                    matrix2 = JSON.parse(tempVal)
-                    //console.log(matrix2)
-                } else {
-                    msg.channel.send(errorCommand[0] + '\n' + errorCommand[2] + '\n' + errorCommand[1])
-                }
-
-            } else if (discordMessage.search(matmulOpt[0]) >= 0 && discordMessage.search(matmulOpt[1]) >= 0) {
-                //both -m1 -m2 input
-                //check if both input are correct
-                const secondInputPos = con.search(matmulOpt[1])
-                const temp1 = con.slice(matmulOpt[0].length + 2, secondInputPos - 1)
-                const temp2 = con.slice(secondInputPos + matmulOpt[1].length + 1, con.length)
-
-                if (temp1.length > matmulOpt[0].length && temp2.length > matmulOpt[1].length) {
-                    //convert string to array
-                    matrix1 = JSON.parse(temp1)
-                    matrix2 = JSON.parse(temp2)
-
-                    //perform matmul
-                    matmulSolve(matrix1, matrix2).then(res => {
-                        //console.log(res)
-                        msg.channel.send('Result: \n'+ res)
-                    })
-
-                } else {
-                    msg.channel.send(errorCommand[0] + '\n' + errorCommand[1] + '\ninput is wrong mofos')
-                }
+                //perform matmul
+                matmulSolve(matrix1, matrix2).then(res => {
+                    //console.log(res)
+                    msg.channel.send('Result: \n' + res)
+                    matrix1 = []
+                    matrix2 = []
+                })
 
             } else {
-                //error in command
-                msg.channel.send(errorCommand[0] + '\n' + errorCommand[1])
+                msg.channel.send(errorCommand[0] + '\n' + errorCommand[1] + '\ninput is wrong mofos')
             }
 
-        } else {
+        }
+        else {
+            //error in command
             msg.channel.send(errorCommand[0] + '\n' + errorCommand[1])
         }
+
+    } else {
+        msg.channel.send(errorCommand[0] + '\n' + errorCommand[1])
+    }
 
 }
 
